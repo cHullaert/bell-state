fe09e731b5aa84b55b2ddacaf225a2db783c51da
Christof Hullaert <christof.hullaert@gmail.com>
2019-09-01 22:51:36 +0200

    initial commit

---
+++ Bell state/Bell.qs
@@ -0,0 +1,191 @@
+﻿namespace Quantum.Bell_state
+{
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Canon;
+    
+	// Unit is similar to void
+	// this code defines a Q# operation, it's the basic unit of quantum execution in Q# (similar to function)
+    operation Set (desired: Result, q1: Qubit) : Unit {
+		// M measures the qbit
+		if (desired != M(q1)) {
+			// X applies a bit flip
+			X(q1);
+		}    
+	}
+
+	// q# use tuples instead of structure or records to pass multiple parameters (Int, Int)
+	operation MeasureQBit (count : Int, initial: Result) : (Int, Int, Int) {
+		// by default variables are immutable
+		// variable types are always inferred by the compiler
+		mutable numOnes = 0;
+
+		// initializatino of qbits
+		using (qubit = Qubit()) {
+
+			// no equivalent for standard for loop, we have to specify a range
+			for (test in 1..count) {
+				Set (initial, qubit);
+
+				// mesure the value of the qbits 
+				let res = M (qubit);
+
+				// Count the number of ones we saw:
+				if (res == One) {
+					set numOnes += 1;
+				}
+			}
+			Set(Zero, qubit);
+		} // qbits are freed here
+
+		// Return number of times we saw a |0> and number of times we saw a |1>
+		return (count-numOnes, numOnes, 0);
+	}
+
+	// q# use tuples instead of structure or records to pass multiple parameters (Int, Int)
+	operation FlipQBit (count : Int, initial: Result) : (Int, Int, Int) {
+		// by default variables are immutable
+		// variable types are always inferred by the compiler
+		mutable numOnes = 0;
+
+		// initializatino of qbits
+		using (qubit = Qubit()) {
+
+			// no equivalent for standard for loop, we have to specify a range
+			for (test in 1..count) {
+				Set (initial, qubit);
+
+				// flip the QBit
+				X(qubit);
+				// mesure the value of the qbits 
+				let res = M (qubit);
+
+				// Count the number of ones we saw:
+				if (res == One) {
+					set numOnes += 1;
+				}
+			}
+			Set(Zero, qubit);
+		} // qbits are freed here
+
+		// Return number of times we saw a |0> and number of times we saw a |1>
+		return (count-numOnes, numOnes, 0);
+	}
+
+	// q# use tuples instead of structure or records to pass multiple parameters (Int, Int)
+	operation SuperposeQBit (count : Int, initial: Result) : (Int, Int, Int) {
+		// by default variables are immutable
+		// variable types are always inferred by the compiler
+		mutable numOnes = 0;
+
+		// initializatino of qbits
+		using (qubit = Qubit()) {
+
+			// no equivalent for standard for loop, we have to specify a range
+			for (test in 1..count) {
+				Set (initial, qubit);
+
+				// put the QBit in superposed state with the Hadamard gate
+				H(qubit);
+				// mesure the value of the qbits 
+				let res = M (qubit);
+
+				// Count the number of ones we saw:
+				if (res == One) {
+					set numOnes += 1;
+				}
+			}
+			Set(Zero, qubit);
+		} // qbits are freed here
+
+		// Return number of times we saw a |0> and number of times we saw a |1>
+		return (count-numOnes, numOnes, 0);
+	}
+
+    operation EntangleQBits (count : Int, initial: Result) : (Int, Int, Int) {
+
+        mutable numOnes = 0;
+        mutable agree = 0;
+        using ((q0, q1) = (Qubit(), Qubit())) {
+            for (test in 1..count)
+            {
+                Set (initial, q0);
+                Set (Zero, q1);
+
+                H(q0);
+                CNOT(q0,q1);
+                let res = M (q0);
+
+                if (M (q1) == res) {
+                    set agree += 1;
+                }
+
+                // Count the number of ones we saw:
+                if (res == One) {
+                    set numOnes += 1;
+                }
+                
+            }
+            
+            Set(Zero, q0);
+            Set(Zero, q1);
+        }
+
+        // Return number of times we saw a |0> and number of times we saw a |1>
+        return (count-numOnes, numOnes, agree);    
+	}
+
+	// send the state of one qubit to a target qubit by teleportation
+	// the state of msg will be collapsed
+	// msg: the qbit we want to send
+	// target: the qbit init with |0> (used to get the state of msg)
+    operation Teleport (msg : Qubit, target : Qubit) : Unit {
+        using (register = Qubit()) {
+            
+			// Entanglement
+            H(register);
+            CNOT(register, target);
+
+            // Encode the message into the entangled pair,
+            // and measure the qubits to extract the classical data
+            // we need to correctly decode the message into the target qubit:
+            CNOT(msg, register);
+            H(msg);
+            let data1 = M(msg);
+            let data2 = M(register);
+
+            // apply correction on message
+            if (data1 == One) { Z(target); }
+            if (data2 == One) { X(target); }
+
+            // Reset our "register"
+            Reset(register);
+        }
+    }
+    
+
+	// message represents the qbit values
+	// return the z-basis measurement on the teleport qbits (converted in bool)
+    operation TeleportClassicalMessage (message : Bool) : Bool {
+
+        using ((msg, target) = (Qubit(), Qubit())) {
+
+            // Encode the message we want to send.
+            if (message) {
+                Set(One, msg);
+            }
+
+            // Use the operation to teleport to target
+            Teleport(msg, target);
+
+            // check the value
+            let measurement = M(target) == One;
+
+            // Reset all qbits
+            Reset(msg);
+            Reset(target);
+
+            return measurement;
+        }
+    }
+}
+
---
+++ Bell state/Driver.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+using System;
+using System.Linq;
+
+namespace Quantum.Bell_state
+{
+    class Driver
+    {
+
+
+        static void runMeasure(QuantumSimulator qsim, Result initial)
+        {
+            var res = MeasureQBit.Run(qsim, 1000, initial).Result;
+            var (numZeros, numOnes, agrees) = res;
+            System.Console.WriteLine(
+                $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4} agree={agrees,-4}");
+        }
+
+        static void runFlip(QuantumSimulator qsim, Result initial)
+        {
+            var res = FlipQBit.Run(qsim, 1000, initial).Result;
+            var (numZeros, numOnes, agrees) = res;
+            System.Console.WriteLine(
+                $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4} agree={agrees,-4}");
+        }
+
+        static void runSuperposition(QuantumSimulator qsim, Result initial)
+        {
+            var res = SuperposeQBit.Run(qsim, 1000, initial).Result;
+            var (numZeros, numOnes, agrees) = res;
+            System.Console.WriteLine(
+                $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4} agree={agrees,-4}");
+        }
+
+        static void runEntanglement(QuantumSimulator qsim, Result initial)
+        {
+            var res = EntangleQBits.Run(qsim, 1000, initial).Result;
+            var (numZeros, numOnes, agrees) = res;
+            System.Console.WriteLine(
+                $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4} agree={agrees,-4}");
+        }
+
+        static void run(int action)
+        {
+            // constructs a quantum simulator
+            using (var qsim = new QuantumSimulator())
+            {
+                // initial values
+                Result[] initials = new Result[] { Result.Zero, Result.One };
+                foreach (Result initial in initials)
+                {
+                    // run 1000 times
+                    // the operation is run asynchronously
+                    // note that operation creates a c# class 
+
+                    switch (action)
+                    {
+                        case 1:
+                            runMeasure(qsim, initial);
+                            break;
+                        case 2:
+                            runFlip(qsim, initial);
+                            break;
+                        case 3:
+                            runSuperposition(qsim, initial);
+                            break;
+                        case 4:
+                            runEntanglement(qsim, initial);
+                            break;
+                    }
+                }
+            }
+        }
+
+        static void estimate(int action)
+        {
+
+            var estimator = new ResourcesEstimator();
+
+            switch (action)
+            {
+                case 6:
+                    var res6 = MeasureQBit.Run(estimator, 1000, Result.Zero).Result;
+                    break;
+                case 7:
+                    var res7 = FlipQBit.Run(estimator, 1000, Result.Zero).Result;
+                    break;
+                case 8:
+                    var res8 = SuperposeQBit.Run(estimator, 1000, Result.Zero).Result;
+                    break;
+                case 9:
+                    var res9 = EntangleQBits.Run(estimator, 1000, Result.Zero).Result;
+                    break;
+            }
+
+            System.Console.WriteLine(estimator.ToTSV());
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("CNOT: The count of CNOT(also known as the Controlled Pauli X gate) gates executed.");
+            System.Console.WriteLine("QubitClifford: The count of any single qubit Clifford and Pauli gates executed.");
+            System.Console.WriteLine("Measure: The count of any measurements executed.");
+            System.Console.WriteLine("R: The count of any single qubit rotations executed, excluding T, Clifford and Pauli gates.");
+            System.Console.WriteLine("T: The count of T gates and their conjugates, including the T gate, T_x = H.T.H, and T_y = Hy.T.Hy, executed.");
+            System.Console.WriteLine("Depth: Depth of the quantum circuit executed by the Q# operation. By default, only T gates are counted in the depth, see depth counter for details.");
+            System.Console.WriteLine("Width: Maximum number of qubits allocated during the execution of the Q# operation.");
+            System.Console.WriteLine("BorrowedWidth: Maximum number of qubits borrowed inside the Q# operation.");
+        }
+
+        static void teleport()
+        {
+            using (var qsim = new QuantumSimulator())
+            {
+                var rand = new System.Random();
+
+                foreach (var idxRun in Enumerable.Range(0, 8))
+                {
+                    var sent = rand.Next(2) == 0;
+                    var received = TeleportClassicalMessage.Run(qsim, sent).Result;
+                    System.Console.WriteLine($"Round {idxRun}:\tSent {sent},\tgot {received}.");
+                    System.Console.WriteLine(sent == received ? "Teleportation successful!!\n" : "\n");
+                }
+            }
+        }
+
+        static void playAction(int action)
+        {
+            if (Enumerable.Range(1, 4).Contains(action))
+            {
+                run(action);
+            }
+            else if (Enumerable.Range(6, 4).Contains(action))
+            {
+                estimate(action);
+            }
+            if (action == 5)
+            {
+                teleport();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("What do you want to do ?");
+            Console.WriteLine("");
+            Console.WriteLine("Quantum simulator");
+            Console.WriteLine("=================");
+            Console.WriteLine("1. Measure qbit");
+            Console.WriteLine("2. Flip qbit");
+            Console.WriteLine("3. Put qbit in superposition state");
+            Console.WriteLine("4. Entangle qbits");
+            Console.WriteLine("5. Teleport qbit");
+            Console.WriteLine("");
+            Console.WriteLine("Resource estimators");
+            Console.WriteLine("===================");
+            Console.WriteLine("6. Measure qbit");
+            Console.WriteLine("7. Flip qbit");
+            Console.WriteLine("8. Put qbit in superposition state");
+            Console.WriteLine("9. Entangle qbits");
+            Console.WriteLine("");
+            Console.WriteLine("Please introduce your choice: ");
+
+            var consoleKey = Console.ReadKey();
+            if (char.IsDigit(consoleKey.KeyChar)) {
+                Console.WriteLine("your choice was: " + consoleKey.KeyChar.ToString());
+                playAction(Int32.Parse(consoleKey.KeyChar.ToString()));
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
